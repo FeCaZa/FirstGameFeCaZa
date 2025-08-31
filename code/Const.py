e2a0f0efaeb116832c1620ba493a14d9209ef7d6
Felipe <f.zalewski92@gmail.com>
2025-08-31 16:44:21 -0300

    Creating asset folder insert all assets Creating the menu: Backgroud,Sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,17 @@
+# C
+COLOR_MENU = (50, 25, 0)
+COLOR_MENU_OP = (250, 250, 230)
+
+
+# M
+
+MENU_TEXT = (' Novo Jogo - 1 Jogador ',
+             ' Novo Jogo - 2 Jogador - Cooperativo ',
+             ' Novo Jogo - 2 Jogador - Competitivo ',
+             'Classificação',
+             'Sair'
+             )
+
+# W
+WIN_WIDTH = 1000
+WIN_HEITH = 561
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+from code.Const import *
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/Menu.wav')
+        pygame.mixer_music.play(-1)
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, 'StarDate', COLOR_MENU, ((WIN_WIDTH/2), 60))
+            self.menu_text(50, '-4821184-',  COLOR_MENU, ((WIN_WIDTH / 2), 110))
+
+            for i in range(len(MENU_TEXT)):
+                self.menu_text(40, MENU_TEXT[i], COLOR_MENU_OP, ((WIN_WIDTH / 2), 300 + 40 * i))
+
+            pygame.display.flip()
+            # Checagem de eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Fechar janela
+                    quit()  # Encerrar pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
