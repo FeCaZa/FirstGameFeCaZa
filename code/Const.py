7b9c42937c01fc59f83dcdd748d4b3b552edbb4f
Felipe <f.zalewski92@gmail.com>
2025-09-01 00:23:54 -0300

    fixing imports in level.py file

--- code/Const.py
+++ code/Const.py
@@ -7,25 +7,25 @@ COLOR_MENU_SELECT = (0, 255, 255)
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 0,
-    'Level1Bg2': 2,
-    'Level1Bg3': 3,
-    'Level1Bg4': 4,
-    'Level1Bg5': 5,
-    'Level1Bg6': 6,
-    'Level1Bg7': 7,
-    'Level1Bg8': 8,
+    'Level1Bg2': 1,
+    'Level1Bg3': 2,
+    'Level1Bg4': 3,
+    'Level1Bg5': 4,
+    'Level1Bg6': 5,
+    'Level1Bg7': 6,
+    'Level1Bg8': 7,
 
 }
 
 # M
 
 MENU_OPTION = (' Novo Jogo - 1 Jogador ',
-             ' Novo Jogo - 2 Jogador - Cooperativo ',
-             ' Novo Jogo - 2 Jogador - Competitivo ',
-             'Classificação',
-             'Sair'
-             )
+               ' Novo Jogo - 2 Jogador - Cooperativo ',
+               ' Novo Jogo - 2 Jogador - Competitivo ',
+               'Classificação',
+               'Sair'
+               )
 
 # W
 WIN_WIDTH = 1000
-WIN_HEITH = 561
+WIN_HEIGHT = 561
--- code/level.py
+++ code/level.py
@@ -1,23 +1,54 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame
+import sys
 
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import COLOR_MENU_OP, WIN_HEIGHT
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 
 
 class Level:
     def __init__(self, window, name, game_mode):
+        self.timeout = None
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000 # 20 segundos
 
     def run(self, ):
+        pygame.mixer_music.load(f'./asset/{self.name}.wav')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Fechar janela
+                    sys.exit()
+
+        # printed text
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_MENU_OP, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_MENU_OP, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}',COLOR_MENU_OP, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
         pass
+
+        # Collisions
+        #EntityMediator.verify_collision(entity_list=self.entity_list)
+        #EntityMediator.verify_health(entity_list=self.entity_list)
+
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
